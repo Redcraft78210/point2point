5183b6729e28419ff38499c43911612aa8e8f787
Redcraft78210 <belaise.clement@gmail.com>
2024-12-21 19:32:08 +0100

    Ajout des options lors de l'appel du binaire client

--- client.c
+++ client.c
@@ -5,8 +5,6 @@
 #include <zlib.h>
 #include <unistd.h>
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 2222
 #define BLOCK_SIZE 4096
 
 void send_file(int socket_fd, const char *filename) {
@@ -51,21 +49,43 @@ void send_file(int socket_fd, const char *filename) {
     printf("File sent successfully.\n");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <IP_ADDRESS> <PORT> <FILE_TO_SEND>\n", argv[0]);
+        return 1;
+    }
+
+    const char *ip_address = argv[1];
+    int port = atoi(argv[2]);
+    const char *filename = argv[3];
+
     int socket_fd;
     struct sockaddr_in server_addr;
 
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
-
-    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Connection to server failed");
+    if (socket_fd < 0) {
+        perror("Socket creation failed");
         return 1;
     }
 
-    printf("Connected to server.\n");
-    send_file(socket_fd, "input_file");
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip_address, &server_addr.sin_addr) <= 0) {
+        perror("Invalid IP address");
+        close(socket_fd);
+        return 1;
+    }
+
+    if (connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection to server failed");
+        close(socket_fd);
+        return 1;
+    }
+
+    printf("Connected to server at %s:%d\n", ip_address, port);
+    send_file(socket_fd, filename);
+
     return 0;
 }
